8e853d871188380064a399af2ed8ffbc171d72bf
livx5199 <livx5199@stud.kea.dk>
2022-11-08 14:41:13 +0100

    fetch products

--- simple-shop/src/App.jsx
+++ simple-shop/src/App.jsx
@@ -1,14 +1,27 @@
+import { useState, useEffect } from "react"
 import Basket from "./components/Basket"
 import Header from "./components/Header"
 import ProductList from "./components/ProductList"
 
 function App() {
 
+  //fetching product data
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("https://kea-alt-del.dk/t7/api/products");
+      const data = await response.json();
+      setProducts(data);
+    }
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <Header />
-      <ProductList />
-      <Basket />
+      <ProductList products={products} />
+      <Basket products={products} />
     </div>
   )
 }
--- simple-shop/src/components/Product.jsx
+++ simple-shop/src/components/Product.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Product() {
+function Product(props) {
   return (
       <article className="Product">
-          <h2>PRODUCTNAME</h2>
-          <p>12334,-</p>
-          <img src="" alt="product" />
+          <h2>{props.productdisplayname}</h2>
+          <p>{props.price},-</p>
+          <img src={`https://kea-alt-del.dk/t7/images/webp/640/${props.id}.webp`} alt={props.productdisplayname} />
           <button>Add to basket</button>
     </article>
   )
