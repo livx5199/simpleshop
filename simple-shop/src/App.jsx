13ca145059f75ec8d94ad922b829315ac9a8b79a
livx5199 <livx5199@stud.kea.dk>
2022-11-08 15:12:26 +0100

    addToBasket

--- simple-shop/src/App.jsx
+++ simple-shop/src/App.jsx
@@ -5,6 +5,25 @@ import ProductList from "./components/ProductList"
 
 function App() {
 
+  //cart
+  const [cart, setCart] = useState([])
+
+  function addToCart(data) {
+    //do we have the product
+    if (cart.find(entry => entry.id === data.id)) {
+      setCart(oldCart => oldCart.map(entry => {
+        if (entry.id !== data.id) {
+          return entry
+        }
+        const copy = { ...entry };
+        copy.amount = copy.amount + 1;
+        return copy;
+      }))
+    } else {
+      setCart(oldCart => oldCart.concat({...data, amount: 1}))
+    }
+  }
+
   //fetching product data
   const [products, setProducts] = useState([])
 
@@ -20,8 +39,8 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <ProductList products={products} />
-      <Basket products={products} />
+      <ProductList products={products} addToCart={addToCart} />
+      <Basket products={products} cart={cart} />
     </div>
   )
 }
--- simple-shop/src/components/Basket.jsx
+++ simple-shop/src/components/Basket.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Basket() {
+function Basket(props) {
   return (
     <section className="Basket">
       <ul>
-        <li>SomeProduct x 3, 1234,-</li>
+        {props.cart.map((item) => (
+         <li>{item.productdisplayname} x {item.amount}, {item.amount * item.price},-</li> 
+        ))}
+        
       </ul>
       </section>
   )
